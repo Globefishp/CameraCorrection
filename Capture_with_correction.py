88e6f128e850bdd3d828673dcb47887ac77ddcfe
Haiyun Huang <globefishp@outlook.com>
2025-07-27 23:20:36 +0800

    V4 implemented a two phase Processor, slightly reduce init overhead by 0.3ms.

--- Capture_with_correction.py
+++ Capture_with_correction.py
@@ -9,13 +9,17 @@ from raw_processing_jit_V3_1 import raw_processing_jit_V3_1
 from raw_processing_jit_V4 import raw_processing_jit_V4
 from raw_processing_jit_V5 import raw_processing_jit_V5
 from raw_processing_cy import raw_processing_cy
+from raw_processing_cy_V2 import raw_processing_cy_V2
+from raw_processing_cy_V2_unroll import raw_processing_cy_V2_unroll
+from raw_processing_cy_V3 import raw_processing_cy_V3
+from raw_processing_cy_V4 import RawV4Processor
 import matplotlib.pyplot as plt
 import time
 import cProfile
 import pstats
 
-current_jit_func = raw_processing_cy
-current_jit_func_name = 'raw_processing_cy'
+current_jit_func = raw_processing_cy_V2
+current_jit_func_name = 'raw_processing_cy_V4'
 
 EXPOSURE_TIME = 10 # ms
 correction_info = np.load('./correction_results.npy', allow_pickle=True).item()
@@ -32,25 +36,74 @@ mycam.open()
 img = mycam.grab_raw()
 
 mycam.close()
-
-srgb_img = current_jit_func(img, 
-                        black_level=32, 
-                        ADC_max_level=4096,
-                        bayer_pattern='BGGR',
-                        wb_params=correction_info['wb_params'],
-                        fwd_mtx=correction_info['fwd_mtx'],
-                        render_mtx=XYZ_TO_SRGB,
-                        gamma='BT709',
-                        )
+if current_jit_func_name == 'raw_processing_cy_V4':
+    processor = RawV4Processor(black_level=32,
+                               ADC_max_level=4096,
+                               bayer_pattern='BGGR',
+                               wb_params=correction_info['wb_params'],
+                               fwd_mtx=correction_info['fwd_mtx'],
+                               render_mtx=XYZ_TO_SRGB,
+                               gamma='BT709',
+                               )
+    srgb_img = processor.process(img)
+else:
+    srgb_img = current_jit_func(img, 
+                            black_level=32, 
+                            ADC_max_level=4096,
+                            bayer_pattern='BGGR',
+                            wb_params=correction_info['wb_params'],
+                            fwd_mtx=correction_info['fwd_mtx'],
+                            render_mtx=XYZ_TO_SRGB,
+                            gamma='BT709',
+                            )
 # Save img using matplotlib
 plt.imsave('srgb_img.png', srgb_img)
 
 # 2. 多次运行并记录时间
-num_runs = 500
+num_runs = 50
 run_times = []
+timings_total = np.zeros(4)
 print(f"\n--- 运行 {num_runs} 次 {current_jit_func_name} 函数并记录时间 ---")
 for _ in range(num_runs):
-    start_time = time.perf_counter()
+    if current_jit_func_name == 'raw_processing_cy_V4':
+        start_time = time.perf_counter()
+        srgb_img = processor.process(img)
+        end_time = time.perf_counter()
+    else:
+        start_time = time.perf_counter()
+        current_jit_func(img,
+                       black_level=32,
+                       ADC_max_level=4096,
+                       bayer_pattern='BGGR',
+                       wb_params=correction_info['wb_params'],
+                       fwd_mtx=correction_info['fwd_mtx'],
+                       render_mtx=XYZ_TO_SRGB,
+                       gamma='BT709',
+                       )
+        end_time = time.perf_counter()
+    # timings_total += timings
+    run_times.append(end_time - start_time)
+
+average_time = sum(run_times) / num_runs
+# timings_average = timings_total / num_runs
+std_dev = np.std(run_times)
+
+print(f"\n--- {current_jit_func_name} 函数运行时间 (avg. ± std) ({num_runs} 次): {average_time*1000:.3f} ± {std_dev*1000:.3f} 毫秒 ---")
+# print("\n--- 性能计时结果 (秒) ---")
+# print(f"白平衡填充 (WB_fill_time): {timings_average[0]:.6f} s")
+# print(f"去马赛克 (Demosaic_time): {timings_average[1]:.6f} s")
+# print(f"色彩矩阵与Gamma (CCM_Gamma_time): {timings_average[2]:.6f} s")
+# print(f"总处理时间 (Total_time): {timings_average[3]:.6f} s")
+
+
+# 3. 详细性能剖析并直接打印结果
+print(f"\n--- {current_jit_func_name} 函数详细性能剖析结果 ---")
+profiler = cProfile.Profile()
+if current_jit_func_name == 'raw_processing_cy_V4':
+    profiler.enable()
+    srgb_img = processor.process(img)
+else:
+    profiler.enable()
     current_jit_func(img,
                    black_level=32,
                    ADC_max_level=4096,
@@ -60,27 +113,6 @@ for _ in range(num_runs):
                    render_mtx=XYZ_TO_SRGB,
                    gamma='BT709',
                    )
-    end_time = time.perf_counter()
-    run_times.append(end_time - start_time)
-
-average_time = sum(run_times) / num_runs
-std_dev = np.std(run_times)
-
-print(f"\n--- {current_jit_func_name} 函数运行时间 (avg. ± std) ({num_runs} 次): {average_time*1000:.3f} ± {std_dev*1000:.3f} 毫秒 ---")
-
-# 3. 详细性能剖析并直接打印结果
-print(f"\n--- {current_jit_func_name} 函数详细性能剖析结果 ---")
-profiler = cProfile.Profile()
-profiler.enable()
-current_jit_func(img,
-               black_level=32,
-               ADC_max_level=4096,
-               bayer_pattern='BGGR',
-               wb_params=correction_info['wb_params'],
-               fwd_mtx=correction_info['fwd_mtx'],
-               render_mtx=XYZ_TO_SRGB,
-               gamma='BT709',
-               )
 profiler.disable()
 
 stats = pstats.Stats(profiler)
